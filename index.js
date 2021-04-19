365ac6d33871ab1889f44285e3d0888b9e12c5a5
juanml21 <78939842+juanml21@users.noreply.github.com>
2021-04-19 18:06:53 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 
 client.on("ready", () => {
-  client.user.setActivity("As Real as It Gets", { type: "WATCHING" });
+  client.user.setActivity("Como se arma un mierdero", { type: "WATCHING" });
   console.log("Listo!");
 });
 
